9a90fd6bd4a912f6378256f6a052ff66ea2419d7
Arifu Islam Adil <adil.islam.arif@gmail.com>
2021-12-25 12:30:24 +0600

    js string array

---
+++ case-convert.js
@@ -0,0 +1,8 @@
+const nameOne = "Abvi Rahman";
+const nameTwo = "abvi rahman";
+
+if (nameOne.toLowerCase() == nameTwo.toLowerCase()) {
+    console.log("name matched");
+} else {
+    console.log("name doesn't matched");
+}
