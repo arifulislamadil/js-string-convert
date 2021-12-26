23258fb934b8958e8a57ef2431522b531c9c87d7
Arifu Islam Adil <adil.islam.arif@gmail.com>
2021-12-27 00:07:17 +0600

    added few lines

---
+++ try-catch.js
@@ -0,0 +1,21 @@
+const name = "ami";
+name = "tumi";
+console.log(name);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+const student;
+console.log(student);
+
+
+let name = "ami";
+try {
+    name = "tumi"
+}
+catch (error) {
+
+}
+console.log(name);
